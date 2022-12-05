5b178f566060f62770398d4f9826e20425d23bf1
Damagucci-Juice <jsr9874@gmail.com>
2022-12-05 10:19:56 +0900

    feat: project init

---
+++ IdusClone/IdusClone/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  IdusClone
+//
+//  Created by YEONGJIN JANG on 2022/12/05.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
