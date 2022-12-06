09aea64c2a5311a024208ab87a243a9e467664e4
Damagucci-Juice <jsr9874@gmail.com>
2022-12-06 22:19:21 +0900

    feat: DefaultSearchBarDelegate 구축 및 info.plist 로 전역변수 선언

---
+++ IdusClone/IdusClone/Presentation/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  IdusClone
+//
+//  Created by YEONGJIN JANG on 2022/12/05.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        let appCode = (Bundle.main.object(forInfoDictionaryKey: "AppCode") as? String) ?? ""
+        searchBar.placeholder = "\(appCode)를 입력해주세요"
+        return searchBar
+    }()
+    
+    public let networkManager = NetworkManager()
+    
+    private let searchBarDelegate = DefaultSearchBarDelegate()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setUpSearchController()
+    }
+    
+    private func setUpSearchController() {
+        self.navigationItem.titleView = searchBar
+        searchBarDelegate.viewController = self
+        searchBar.delegate = self.searchBarDelegate
+    }
+    
+}
