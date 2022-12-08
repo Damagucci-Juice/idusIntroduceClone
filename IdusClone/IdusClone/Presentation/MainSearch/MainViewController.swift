e49c0c5d435e4efcf7855e41f0d9aae24325efb5
Damagucci-Juice <jsr9874@gmail.com>
2022-12-08 17:20:38 +0900

    feat: RatingReviewView 텍스트 부분 구현

--- IdusClone/IdusClone/Presentation/MainSearch/MainViewController.swift
+++ IdusClone/IdusClone/Presentation/MainSearch/MainViewController.swift
@@ -31,13 +31,20 @@ final class MainViewController: UIViewController {
 //            make.width.equalTo(100)
 //            make.height.equalTo(50)
 //        }
-        let progressView = ProgressBarView(progressRating: 0.2)
-        view.addSubview(progressView)
-        progressView.snp.makeConstraints { make in
-            make.leading.trailing.centerY.equalToSuperview()
-            make.height.equalTo(10)
-        }
+//        let progressView = ProgressBarView(progressRating: 0.2)
+//        view.addSubview(progressView)
+//        progressView.snp.makeConstraints { make in
+//            make.leading.trailing.centerY.equalToSuperview()
+//            make.height.equalTo(10)
+//        }
+//
+        let appDetail = AppIntroduction(id: 123, appName: "123", sellerName: "123", isGameCenterEnabled: false, screenshotURLs: [], artworkULR60: URL(string: "123")!, artworkULR512: URL(string: "123")!, artworkULR100: URL(string: "123")!, releaseNotes: "123", artistID: 123, artistName: "123", genres: [], price: 0.0, resultDescription: "123", primaryGenreName: "123", primaryGenreID: 123, currentVersionReleaseDate: Date.now, contentAdvisoryRating: "123", trackContentRating: "123", minimumOSVersion: "", languageCodes: [], fileSizeBytes: "", sellerURL: URL(string: "123")!, formattedPrice: "123", averagedUserRating: 4.7924325734, userRatingCount: 239, version: "123123423")
         
+        let ratingView = RatingReviewView(appDetail: appDetail)
+        self.view.addSubview(ratingView)
+        ratingView.snp.makeConstraints { make in
+            make.leading.top.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     private func setUpSearchController() {
