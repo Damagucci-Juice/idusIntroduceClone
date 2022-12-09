578cbf04c4ccc66a3081c34b83d1fee9ee52e283
Damagucci-Juice <jsr9874@gmail.com>
2022-12-09 17:14:14 +0900

    feat: 수평 스크롤뷰 구현
    - 리뷰를 담는 용도

--- IdusClone/IdusClone/Presentation/MainSearch/MainViewController.swift
+++ IdusClone/IdusClone/Presentation/MainSearch/MainViewController.swift
@@ -19,32 +19,41 @@ final class MainViewController: UIViewController {
     
     private let searchBarDelegate = DefaultSearchBarDelegate()
     
+    private let scrollView = UIScrollView().then { scrollView in
+        scrollView.isPagingEnabled = true
+    }
+    
+    private let scrollViewContainer = UIStackView().then { stackView in
+        stackView.axis = .horizontal
+        stackView.spacing = Const.mediumSpacing
+    }
+    
+    //MARK: - Start 3, This 3 views will disappear.
+    private let redView = UIView().then { view in
+        view.backgroundColor = .red
+        view.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+    }
+    private let blueView = UIView().then { view in
+        view.backgroundColor = .blue
+        view.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+    }
+    private let greenView = UIView().then { view in
+        view.backgroundColor = .green
+        view.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+    }
+    // MARK: - End 3
+    
+    let appDetail = AppIntroduction(id: 123, appName: "123", sellerName: "123", isGameCenterEnabled: false, screenshotURLs: [], artworkULR60: URL(string: "123")!, artworkULR512: URL(string: "123")!, artworkULR100: URL(string: "123")!, releaseNotes: "123", artistID: 123, artistName: "123", genres: [], price: 0.0, resultDescription: "123", primaryGenreName: "123", primaryGenreID: 123, currentVersionReleaseDate: Date.now, contentAdvisoryRating: "123", trackContentRating: "123", minimumOSVersion: "", languageCodes: [], fileSizeBytes: "", sellerURL: URL(string: "123")!, formattedPrice: "123", averagedUserRating: 4.7924325734, userRatingCount: 239, version: "123123423")
+    
+    lazy var ratingView = RatingProgressView(appDetail: appDetail)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setUpSearchController()
-//        let starView = FiveStarView(ratingAmount: 4.0, isContinues: true)
-//        self.view.addSubview(starView)
-//
-//        starView.snp.makeConstraints { make in
-//            make.center.equalToSuperview()
-//            make.width.equalTo(100)
-//            make.height.equalTo(50)
-//        }
-//        let progressView = ProgressBarView(progressRating: 0.2)
-//        view.addSubview(progressView)
-//        progressView.snp.makeConstraints { make in
-//            make.leading.trailing.centerY.equalToSuperview()
-//            make.height.equalTo(10)
-//        }
-//
-        let appDetail = AppIntroduction(id: 123, appName: "123", sellerName: "123", isGameCenterEnabled: false, screenshotURLs: [], artworkULR60: URL(string: "123")!, artworkULR512: URL(string: "123")!, artworkULR100: URL(string: "123")!, releaseNotes: "123", artistID: 123, artistName: "123", genres: [], price: 0.0, resultDescription: "123", primaryGenreName: "123", primaryGenreID: 123, currentVersionReleaseDate: Date.now, contentAdvisoryRating: "123", trackContentRating: "123", minimumOSVersion: "", languageCodes: [], fileSizeBytes: "", sellerURL: URL(string: "123")!, formattedPrice: "123", averagedUserRating: 4.7924325734, userRatingCount: 239, version: "123123423")
-        
-        let ratingView = RatingReviewView(appDetail: appDetail)
-        self.view.addSubview(ratingView)
-        ratingView.snp.makeConstraints { make in
-            make.leading.top.equalTo(view.safeAreaLayoutGuide)
-        }
+        setupAddSubView()
+        setupLayout()
+        setupAttribute()
     }
     
     private func setUpSearchController() {
@@ -52,5 +61,45 @@ final class MainViewController: UIViewController {
         searchBarDelegate.viewController = self
         searchBar.delegate = self.searchBarDelegate
     }
+}
+
+extension MainViewController {
+    private func setupAddSubView() {
+        self.view.addSubview(ratingView)
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(scrollViewContainer)
+        scrollViewContainer.addArrangedSubviews([blueView, redView, greenView])
+    }
     
+    private func setupLayout() {
+        ratingView.snp.makeConstraints { make in
+            make.leading.top.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(150)
+        }
+        
+        scrollView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(ratingView).inset(Const.mediumSpacing)
+            make.top.equalTo(ratingView.snp.bottom)
+            make.height.equalTo(200)
+        }
+        
+        scrollViewContainer.snp.makeConstraints { make in
+            make.leading.trailing.top.bottom.equalTo(scrollView)
+        }
+        
+        redView.snp.makeConstraints { make in
+            make.width.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.90)
+            make.height.equalTo(scrollView)
+        }
+        blueView.snp.makeConstraints { make in
+            make.width.equalTo(redView)
+        }
+        greenView.snp.makeConstraints { make in
+            make.width.equalTo(redView)
+        }
+    }
+    
+    private func setupAttribute() {
+        
+    }
 }
